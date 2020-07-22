193af42e39cbc909e9539747c1988d302e7b7de9
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-07-22 08:39:38 +0300

    express added 4

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const http = require('http');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<app-root-package.json>'));
+app.use(express.static(__dirname + '/dist/covid-ang'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<app-root-package.json>/'));
+res.sendFile(path.join(__dirname));
 });
 
 // Start the app by listening on the default Heroku port
