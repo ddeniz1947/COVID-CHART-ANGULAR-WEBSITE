1e2f145eddc34234af8aadecbfc732c51cb675bc
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-07-22 08:35:44 +0300

    express added 3

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<app-root>'));
+app.use(express.static(__dirname + '/dist/<app-root-package.json>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<app-root>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<app-root-package.json>/'));
 });
 
 // Start the app by listening on the default Heroku port
